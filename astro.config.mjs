96c2699675d13c2e4d38876d2c64e491bbc6c4ad
ZaneMasters <angel.pro98@gmail.com>
2026-09-23 13:26:57 -0500

    feat: add SeoContent component and Layout with schema.org structured data

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mejoratusrendimientos.com',
+  trailingSlash: 'always',
   integrations: [
     react(),
     sitemap({
